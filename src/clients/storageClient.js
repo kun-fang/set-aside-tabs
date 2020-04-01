48d4b97e9b5054c003475a3c844a2563974dbfa5
Kun Fang <kfang_anqing@yahoo.com>
2020-04-01 19:32:19 -0400

    changed to use sync storage

--- src/clients/storageClient.js
+++ src/clients/storageClient.js
@@ -1,14 +1,23 @@
 export default {
   get: async function (key) {
-    let result = await browser.storage.local.get(key);
-    return result[key];
+    let [localResult, syncResult] = await Promise.all([
+      browser.storage.local.get(key),
+      browser.storage.sync.get(key)
+    ]);
+    // migrate from local storage to sync storage
+    if (!!localResult[key] && !syncResult[key]) {
+      await this.set(key, localResult[key]);
+      await browser.storage.local.remove(key);
+      syncResult = localResult;
+    }
+    return syncResult[key];
   },
   set: async function (key, value) {
     let result = {};
     result[key] = value;
-    return await browser.storage.local.set(result);
+    return await browser.storage.sync.set(result);
   },
   remove: async function (key) {
-    return await browser.storage.local.remove(key);
+    return await browser.storage.sync.remove(key);
   }
 };
